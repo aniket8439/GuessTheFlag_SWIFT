3af88e6442aa1c6ed6056c0b3af16e705724c75e
Aniket Sharma <aniket8439@gmail.com>
2024-04-11 10:41:57 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Aniket Sharma on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
